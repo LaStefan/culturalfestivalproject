88928c18a4d884e3e2284427058e0ad0d55598e3
Lazarevic,Stefan S <s.lazarevic@student.fontys.nl>
2018-09-24 11:20:13 +0200

    GUI Updates

---
+++ Applications/Check-In,Out/Check-InOutApp/Check-InOutApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Check_InOutApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPersons = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnIN = new System.Windows.Forms.Button();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPersons
+            // 
+            this.lbPersons.FormattingEnabled = true;
+            this.lbPersons.Location = new System.Drawing.Point(172, 12);
+            this.lbPersons.Name = "lbPersons";
+            this.lbPersons.Size = new System.Drawing.Size(179, 329);
+            this.lbPersons.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(384, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnIN
+            // 
+            this.btnIN.BackColor = System.Drawing.Color.Red;
+            this.btnIN.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIN.Location = new System.Drawing.Point(29, 290);
+            this.btnIN.Name = "btnIN";
+            this.btnIN.Size = new System.Drawing.Size(122, 37);
+            this.btnIN.TabIndex = 3;
+            this.btnIN.Text = "Check-IN";
+            this.btnIN.UseVisualStyleBackColor = false;
+            // 
+            // btnOut
+            // 
+            this.btnOut.BackColor = System.Drawing.Color.Blue;
+            this.btnOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOut.Location = new System.Drawing.Point(384, 290);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(122, 37);
+            this.btnOut.TabIndex = 4;
+            this.btnOut.Text = "Check-OUT";
+            this.btnOut.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(384, 88);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(122, 34);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "SearchForPerson";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(539, 353);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.btnIN);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbPersons);
+            this.Name = "Form1";
+            this.Text = "Check-IN/OUT Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbPersons;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnIN;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
