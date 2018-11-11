d03132af46b8077f014099acc574ffd97d632c83
Cheperkova,Nadya N <n.cheperkova@student.fontys.nl>
2018-11-11 16:12:34 +0100

    Website modified

--- Website/HTML/Account_js.js
+++ Website/HTML/Account_js.js
@@ -1,30 +1,36 @@
+var payment = document.getElementsByClassName('paymentMain')[0];
+var account = document.getElementsByClassName('form')[0];
+
+var called = false;
+
+
+
+
 $('.form').find('input').on('keyup blur focus', function (e) {
   
   var $this = $(this),
       label = $this.prev('label');
-
 	  if (e.type === 'keyup') {
 			if ($this.val() === '') {
           label.removeClass('active highlight');
         } else {
           label.addClass('active highlight');
-        }
+            }
     } else if (e.type === 'blur') {
     	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
+    		label.removeClass('active highlight');
 			} else {
-		    label.removeClass('highlight');   
-			}   
+		    label.removeClass('highlight');
+			}
     } else if (e.type === 'focus') {
       
       if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
+    		label.removeClass('highlight');
+			}
       else if( $this.val() !== '' ) {
 		    label.addClass('highlight');
-			}
+      }
     }
-
 });
 
 $('.tab a').on('click', function (e) {
@@ -41,3 +47,23 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
   
 });
+
+function switchPanels() {
+    var pass = document.getElementsByClassName('passwordInput')[0].value;
+    var email = document.getElementsByClassName('emailInput')[0].value;
+    var fn = document.getElementsByClassName('fnInput')[0].value;
+    var ln = document.getElementsByClassName('lnInput')[0].value;
+
+    if(!isEmpty(pass) && !isEmpty(email) && !isEmpty(fn) && !isEmpty(ln)) {
+        called = true;
+    }
+    if(called == true) {
+        account.style.display = 'none';
+        payment.style.display = 'block';
+    }
+
+}
+
+function isEmpty(str){
+    return !str.replace(/\s+/, '').length;
+}
--- Website/HTML/accountSwitch.js
+++ Website/HTML/accountSwitch.js
@@ -1,8 +1,7 @@
-var messageBox = document.getElementsByClassName('messageBox')[0];
+var payment = document.getElementsByClassName('paymentMain')[0];
 var account = document.getElementsByClassName('form')[0];
 
 function switchPanel() {
-    console.log(555)
     account.style.display = 'none';
-    messageBox.style.display = 'flex';
+    payment.style.display = 'block';
 }
