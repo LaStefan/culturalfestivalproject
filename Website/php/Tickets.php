e03c2b52b12d2013801e648996dfca64f597f86e
Cheperkova,Nadya N <n.cheperkova@student.fontys.nl>
2018-12-07 09:32:06 +0100

    Account form modified

--- Website/php/Tickets.php
+++ Website/php/Tickets.php
@@ -198,6 +198,225 @@ session_start();
         <figure>
             <input type="button" name="previous" class="previousMain" value="Previous"/>
             <input type="button" name="next" class="nextMain" id="nextFive" value="Next"/>
+    
+    <div id="TicketRe">
+        <div class="formRe">
+       <div id="AccountForm">
+
+	<ul id="progressAcc">
+		<li class="active"></li>
+		<li></li>
+		<li></li>
+<li></li>
+<li></li>
+<li></li>
+	</ul>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Create an account</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" class="next action-button" value="Register next person" />
+	</fieldset>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Person 2</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" class="next action-button" value="Register next person" />
+		<input type="button" id="previousBt" name="previous" class="previous action-button" value="Previous" />
+	</fieldset>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Person 3</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" class="next action-button" value="Register next person" />
+		<input type="button" id="previousBt" name="previous" class="previous action-button" value="Previous" />
+	</fieldset>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Person 4</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" class="next action-button" value="Register next person" />
+		<input type="button" id="previousBt" name="previous" class="previous action-button" value="Previous" />
+</fieldset>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Person 5</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" class="next action-button" value="Register next person" />
+		<input type="button" id="previousBt" name="previous" class="previous action-button" value="Previous" />
+	</fieldset>
+	<fieldset id="FormAcc">
+		<h2 class="accTitle">Person 6</h2>
+		<div class="top-row">
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                First Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label id="Accountlabel">
+                Last Name<span class="req">*</span>
+              </label>
+              <input class="Accountinput" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Email Address<span class="req">*</span>
+            </label>
+            <input class="Accountinput" type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label id="Accountlabel">
+              Set a password<span class="req">*</span>
+            </label>
+            <input  class="Accountinput" type="password" required autocomplete="off"/>
+          </div>
+		<input type="button" id="nextBt" name="next" value="Register next person" />
+		<input type="button" id="previousBt" name="previous" class="previous action-button" value="Previous" />
+	</fieldset>
+	</div>   
+        </div>
+     </div>
         </figure>
 
         <!-- Panel 6 -->
