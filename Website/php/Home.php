dd4d395e111d7d85a9c8ff8855f2bf4177aa4f59
Cheperkova,Nadya N <n.cheperkova@student.fontys.nl>
2018-12-18 14:45:35 +0100

    Website Home Page

--- Website/php/Home.php
+++ Website/php/Home.php
@@ -3,11 +3,13 @@
 <head>
     
 <link rel="stylesheet" href="css/styles.css">    
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap.css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">    
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">   
+<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    
+        
  
    <title>HOME</title>    
     
@@ -29,9 +31,9 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a id="n1"href="#1">About</a></li>
         <li><a id="n2" href="#2">Countries</a></li>  
-        <li><a id="n3"href="#3">Location</a></li>
-        <li><a id="n4" href="#4">Program</a></li> 
-        <li><a href="Tickets.php">Tickets</a></li>
+        <li><a id="n3"href="#3">Program</a></li>
+        <li><a id="n5" href="#4">Location</a></li>   
+        <li><a href="Tickets.html">Tickets</a></li>
         <li class="dropdown">
           <a href="#logIn" class="dropdown-toggle" data-toggle="dropdown" style="color:#6e9987;">Log In<span class="caret"></span></a>
 			<ul id="loginM" class="dropdown-menu">
@@ -68,33 +70,318 @@
     </header>
     
     <body>
-        <section id="1"><h1>About</h1>
-    
-   
-        <div class="containerH">
-            <div class="row">
-                
-                </div>
-            </div>
+        <section id="1" style="color: #6e9987; margin-top: 1.5%;">
+    <div class="container-fluid text-center bg-grey">
+    <div style="color: red;" class="row text-center">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/festivalimg5.jpg">
+        <p><strong>Amsterdam</strong></p>
+        <p>2017</p>
+      </div>
+    </div>
+    <div style="color: red;" class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/festival.jpg">
+           <p><strong>Eindhoven</strong></p>
+        <p>2019</p>
+      </div>
+    </div>
+    <div style="color: red;" class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/festivalimg4.jpg">
+        <p><strong>Rotterdam</strong></p>
+        <p>2018</p>
+      </div>
+    </div>
+</div>
+              
+            <h2>The Cultural festival is an annual event held in the Netherlands.</h2>
+<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+  <ol class="carousel-indicators" style="z-index: 1;">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+    <h4>This is an event that showcase the beautiful variety of cultures <br> around the world, it is mostly done in a way
+                    where people show their cultures by means of clothes,</h4>
+    </div>
+    <div class="item">
+      <h4>food, music, traditional costumes,<br>
+                    dance steps, languages and many more.</h4>
+    </div>
+    <div class="item">
+      <h4>Everything from folk art to metal music and modern art to<br>
+                    natural beauty is the focus of celebrations in the various festivals of Europe.</h4>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+        </div></div>
                                </section>
         
        
         
-<section id="2"><h1>Countries</h1>
-    
-    
+<section id="2" style="background:#fff;"><h1>Countries</h1>
+   <div class="container-fluid text-center">
+  <h4>EUROPEAN CULTURAL FESTIVAL 2019: participating countries</h4>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="images/bgFlag.png" width="80px">
+      <h4>BULGARIA</h4>
+      <p>"Съединението прави силата"</p>
+    </div>
+    <div class="col-sm-4">
+       <img src="images/ndFlag.png" width="80px">
+      <h4>NETHERLANDS</h4>
+      <p>"Je maintiendrai; Ik zal handhaven"</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/sbFlag.png" width="80px">
+      <h4>SERBIA</h4>
+      <p>"Само слоба Србина спашава"</p>
+    </div>
+    </div>
+    <br><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="images/gmFlag.png" width="80px">
+      <h4>GERMANY</h4>
+      <p>"Einigkeit und Recht und Freiheit"</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/belFlag.jpg" width="80px">
+      <h4>BELGIUM</h4>
+      <p>"L'union fait la force and Einigkeit macht stark"</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/fcFlag.png" width="80px">
+      <h4>FRANCE</h4>
+      <p>"Liberté, égalité, fraternité"</p>
+    </div>
+  </div>
+       <br><br>
+       <div class="row">
+    <div class="col-sm-4">
+      <img src="images/spFlag.png" width="80px">
+      <h4>SPAIN</h4>
+      <p>"Plus Ultra"</p>
+    </div>
+    <div class="col-sm-4">
+       <img src="images/grFlag.png" width="80px">
+      <h4>GREECE</h4>
+      <p>"Ελευθερία ή θάνατος"</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/swFlag.svg" width="80px">
+      <h4>SWEDEN</h4>
+      <p>"För Sverige - i tiden"</p>
+    </div>
+    </div>
+    <br><br>
+</div>
     </section>
-<section id="3"><h1>Location</h1></section>
-<section id="4"><h1>Program</h1></section>
- 
+<section id="3" style="color:#6e9987;"><h1>Program</h1>
         
+    <div class="container" style="width: 100%;">
+  <div class="row">
+    <div class="col-12">
+      <div class="row day-columns">
+        <div class="day-column">
+          <div class="day-header"><strong>MONDAY</strong></div>
+          <div class="day-content">
+            <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span><strong>Music</strong></span>
+                  <span>10:00</span>
+                </footer>
+            </div>
+             <div class="event green" style="background-color: #6e9987;">
+                <span class="title">BULGARIA</span>
+                <footer>
+                  <span>4 Assets</span>
+                  <span>12:00</span>
+                </footer>
+            </div>  
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>14:00</span>
+                </footer>
+            </div>
+              <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>16:00</span>
+                </footer>
+            </div>
+              <div class="event green" style="background-color: #6e9987;">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>18:00</span>
+                </footer>
+            </div>
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>20:00</span>
+                </footer>
+            </div>
+            <div class="event red">
+                <span class="title">NETHERLANDS</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>22:00</span>
+                </footer>
+            </div>
+          </div>
+          </div>
+        <div class="day-column">
+          <div class="day-header"><strong>TUESDAY</strong></div>
+          <div class="day-content">
+            <div class="event red">
+                <span class="title">SERBIA</span>
+                <footer>
+                  <span><strong>Music</strong></span>
+                  <span>10:00</span>
+                </footer>
+            </div>
+             <div class="event green" style="background-color: #6e9987;">
+                <span class="title">BULGARIA</span>
+                <footer>
+                  <span>4 Assets</span>
+                  <span>12:00</span>
+                </footer>
+            </div>  
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>14:00</span>
+                </footer>
+            </div>
+              <div class="event red">
+                <span class="title">NETHERLANDS</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>16:00</span>
+                </footer>
+            </div>
+              <div class="event green" style="background-color: #6e9987;">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>18:00</span>
+                </footer>
+            </div>
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>20:00</span>
+                </footer>
+            </div>
+            <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>22:00</span>
+                </footer>
+            </div>
+          </div>
+         </div>
+        <div class="day-column">
+          <div class="day-header"><strong>WEDNESDAY</strong></div>
+          <div class="day-content">
+            <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span><strong>Music</strong></span>
+                  <span>10:00</span>
+                </footer>
+            </div>
+             <div class="event green" style="background-color: #6e9987;">
+                <span class="title">BULGARIA</span>
+                <footer>
+                  <span>4 Assets</span>
+                  <span>12:00</span>
+                </footer>
+            </div>  
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>14:00</span>
+                </footer>
+            </div>
+              <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>16:00</span>
+                </footer>
+            </div>
+              <div class="event green" style="background-color: #6e9987;">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>3 Assets</span>
+                  <span>18:00</span>
+                </footer>
+            </div>
+            <div class="event grey">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>20:00</span>
+                </footer>
+            </div>
+            <div class="event red">
+                <span class="title">Event Name</span>
+                <footer>
+                  <span>Prop1</span>
+                  <span>22:00</span>
+                </footer>
+            </div>
+          </div>
+          </div></div></div></div></div>
+      
+        </section>
+        
+
+        
+<section id="4" style="color: #6e9987"><h1>Location</h1>
+
+        	
+<iframe style="width: 100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2410.0048978228433!2d6.72150141578757!3d52.84029677987616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e010cfe19b33%3A0xe1bd06427483d7af!2sMolecaten+Park+Kuierpad!5e0!3m2!1snl!2snl!4v1545129487353" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </section>
+    
         <a id="toTop" href="#0"><div class="to-top"></div></a>
-        
-       </body>
+ </body>   
+   <footer id="footerButtom">
+     <div>&copy;CULTURAL FESTIVAL 2018. All rights reserved. Made by DevelopPro.</div>
+    </footer>
+       
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="js/home.js"></script>
-<script src="js/slider.js"></script>    
-        
+<script src="js/slider.js"></script>  
+  
  </html>
